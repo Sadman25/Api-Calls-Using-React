0491f324f42ac76fab79c7ee2fa8cd2017369b65
Shadman Saqif <shadman.saqif@gmail.com>
2022-03-09 11:23:40 +0600

    added verification for Quran API

--- src/components/Quran/Quran.js
+++ src/components/Quran/Quran.js
@@ -1,11 +1,19 @@
 import React, { useState } from 'react'
 import axios from 'axios';
-//import './Quran.css'
 export default function QuranAPI() {
   
   const [chapter,setChapter] = useState('');
   const [data,setData] = useState();
   const url = `https://api.quran.com/api/v4/chapters/${chapter}?language=en`
+
+  const handleChapter= (e)=>{
+    if(e.target.value>114 || e.target.value<1){
+      window.alert('The input should be between 1 and 114')
+    }else{
+      setChapter(e.target.value)
+    }
+    
+  }
 
   const search = (e)=> {
     if (e.key ==='Enter'){
@@ -27,7 +35,7 @@ export default function QuranAPI() {
                   placeholder='Enter between 1-114'
                   value={chapter}
                   type="number"
-                  onChange={(e)=> setChapter(e.target.value)}
+                  onChange={handleChapter}
                   onKeyPress={search} />
                 </div>
                 <div className='place'>
